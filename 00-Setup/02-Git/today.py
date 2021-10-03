aabf2c5e991a24623ac3545ce7e2d6247fa4460a
Meiling Wang <miranda0120@hotmail.com>
2021-10-03 20:40:03 +0100

    Implement my_name_is function

--- 00-Setup/02-Git/today.py
+++ 00-Setup/02-Git/today.py
@@ -1,6 +1,10 @@
 '''
 This is the module docstring
 '''
+def my_name_is():
+    ''' This is the function docstring '''
+    return "miranda0120"
 
-
-pass  # YOUR CODE HERE
+def my_buddy_is():
+    ''' This is the function docstring'''
+    return "miranda0120"
