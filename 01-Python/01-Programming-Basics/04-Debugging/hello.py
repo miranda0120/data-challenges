618315b989eee6d42b900fceeaad4494eff5db5f
Meiling Wang <miranda0120@hotmail.com>
2021-10-06 11:05:12 +0100

    Sucessfully debug the issue

--- 01-Python/01-Programming-Basics/04-Debugging/hello.py
+++ 01-Python/01-Programming-Basics/04-Debugging/hello.py
@@ -3,10 +3,14 @@
 import sys
 
 def full_name(first_name, last_name):
-    """returns the full name"""
-    name = f"{first_name.capitalize()} {last_name.capitalize()}"
+    # """returns the full name"""
+    if first_name == "":
+        return last_name.capitalize().strip()
+    elif last_name == "":
+        return first_name.capitalize().strip()
+    else:
+        return f"{first_name.capitalize().strip()} {last_name.capitalize().strip()}"
 
-    return name
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
