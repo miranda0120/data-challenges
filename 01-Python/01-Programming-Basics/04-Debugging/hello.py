4635f2b8a5467be71b252015105a9a50d5ae674c
Meiling Wang <miranda0120@hotmail.com>
2021-10-06 11:14:48 +0100

    Debugging code update to get the rate at 10.00

--- 01-Python/01-Programming-Basics/04-Debugging/hello.py
+++ 01-Python/01-Programming-Basics/04-Debugging/hello.py
@@ -1,16 +1,16 @@
 # pylint: disable=missing-docstring
 
 import sys
-
+#
 def full_name(first_name, last_name):
-    # """returns the full name"""
-    if first_name == "":
-        return last_name.capitalize().strip()
-    elif last_name == "":
-        return first_name.capitalize().strip()
-    else:
-        return f"{first_name.capitalize().strip()} {last_name.capitalize().strip()}"
-
+    """returns the full name"""
+    name = f"{first_name.capitalize()}{last_name.capitalize()}"
+    # $DELETE_BEGIN
+    # Uncomment the line below and run your code to see the debugger in action
+    # import ipdb; ipdb.set_trace()
+    name = f"{first_name.capitalize()} {last_name.capitalize()}".strip()
+    # $DELETE_END
+    return name
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
