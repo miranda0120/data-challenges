adfa7dc2d8a2da39d020419c05cf87f4315a7d91
Meiling Wang <miranda0120@hotmail.com>
2021-10-06 12:09:21 +0100

    Solved calc.py

--- 01-Python/01-Programming-Basics/05-System-Parameters/calc.py
+++ 01-Python/01-Programming-Basics/05-System-Parameters/calc.py
@@ -1,12 +1,25 @@
 # pylint: disable=missing-module-docstring,missing-function-docstring,eval-used
 import sys
 
-
 def main():
     """Implement the calculator"""
-    pass  # YOUR CODE HERE
+
+    arg1 = int(sys.argv[1])
+    arg2 = sys.argv[2]
+    arg3 = int(sys.argv[3])
+
+    if arg2 == '+':
+        return arg1 + arg3
+    elif arg2 == '-':
+        return arg1 - arg3
+    elif arg2 == '*':
+        return arg1 * arg3
+
+
+
+
+
 
 
 if __name__ == "__main__":
     print(main())
-    
