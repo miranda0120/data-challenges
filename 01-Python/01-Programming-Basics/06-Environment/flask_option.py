f312e9c6a4b8bb6424452f895924b14dc0eb2ed0
Meiling Wang <miranda0120@hotmail.com>
2021-10-06 13:07:48 +0100

    Partial test passed

--- 01-Python/01-Programming-Basics/06-Environment/flask_option.py
+++ 01-Python/01-Programming-Basics/06-Environment/flask_option.py
@@ -4,7 +4,11 @@ import os
 
 def start():
     """returns the right message"""
-    pass  # YOUR CODE HERE
+    FLASK_ENV= ["development", "production"]
+    if os.environ['FLASK_ENV'] == "development" or os.environ['FLASK_ENV'] == "production":
+        return "Starting in development mode..."
+
+    return "Starting in production mode..."
 
 if __name__ == "__main__":
     print(start())
