f1e44502f9cb3b7a3f0cffbe4217552cae480ec7
Meiling Wang <miranda0120@hotmail.com>
2021-10-06 18:04:08 +0100

    code update

--- 01-Python/01-Programming-Basics/04-Debugging/hello.py
+++ 01-Python/01-Programming-Basics/04-Debugging/hello.py
@@ -4,13 +4,10 @@ import sys
 #
 def full_name(first_name, last_name):
     """returns the full name"""
-    name = f"{first_name.capitalize()}{last_name.capitalize()}"
+    name = f"{first_name.capitalize()} {last_name.capitalize()}"
     # $DELETE_BEGIN
-    # Uncomment the line below and run your code to see the debugger in action
-    # import ipdb; ipdb.set_trace()
-    name = f"{first_name.capitalize()} {last_name.capitalize()}".strip()
     # $DELETE_END
-    return name
+    return name.strip()
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
