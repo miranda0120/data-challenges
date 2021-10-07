8c8f1d9122b081b7a5112bd0842e47ef1a14e904
Meiling Wang <miranda0120@hotmail.com>
2021-10-07 20:38:46 +0100

    Sudoku checker worked

--- 01-Python/01-Programming-Basics/07-Optional-Sudoku/sudoku.py
+++ 01-Python/01-Programming-Basics/07-Optional-Sudoku/sudoku.py
@@ -2,4 +2,41 @@
 
 
 def sudoku_validator(grid):
-    pass  # YOUR CODE HERE
+    if not grid:
+        return False
+
+    #rows by rows checking
+    row = set()
+    for i in range(9):
+        for j in range(9):
+            if grid[i][j] in row:
+                return False
+            else:
+                row.add(grid[i][j])
+        row = set()
+
+    #cols by cols checking
+    row = set()
+    for i in range(9):
+        for j in range(9):
+            if grid[j][i] in row:
+                return False
+            else:
+                row.add(grid[j][i])
+        row = set()
+
+    # 3 by 3 check
+    subs = [range(0,3), range(3,6), range(6,9)]
+    subgrids = []
+    for x in subs:
+        for y in subs:
+            subgrids.append([x,y])
+    for (row_range, column_range) in subgrids:
+        row = set()
+        for i in row_range:
+            for j in column_range:
+                if grid[i][j] in row:
+                    return False
+                else:
+                    row.add(grid[i][j])
+    return True
