b6787dbd45b206e1b0ec34347d0a6d5500185a06
Meiling Wang <miranda0120@hotmail.com>
2021-10-10 14:43:56 +0100

    Code updated

--- 01-Python/02-Data-Sourcing/01-CSV/phone_book.py
+++ 01-Python/02-Data-Sourcing/01-CSV/phone_book.py
@@ -1,12 +1,14 @@
 import csv
 
 with open('data/phone_book.csv', mode='r') as csv_file:
-    # csv_reader = csv.reader(csv_file, skipinitialspace=True)
-    # line_count = 0
+    csv_reader = csv.reader(csv_file, skipinitialspace=True)
+    line_count = 0
 
-    # for row in csv_reader:
-        # line_count += 1
-        # print(f"{row[1]}: {row[2]}")
+    for row in csv_reader:
+        if line_count > 1:
+            print(f"{row[1]}: {row[2]}")
+        line_count += 1
+# optional:
     csv_reader = csv.DictReader(csv_file, skipinitialspace=True)
     for row in csv_reader:
         print(f"{row['last_name']}: {row['phone_number']}")
