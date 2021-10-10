f1e42c89532908b8c4f1f4563aa0b7b75c47fd5e
Meiling Wang <miranda0120@hotmail.com>
2021-10-10 21:03:41 +0100

    code updated

--- 01-Python/02-Data-Sourcing/02-API/history.py
+++ 01-Python/02-Data-Sourcing/02-API/history.py
@@ -2,20 +2,43 @@
 
 import sys
 import datetime
-
+import csv
+import urllib.parse
+import requests
+# $DELETE_END
 from weather import search_city
+# $DELETE_BEGIN
+from weather import BASE_URI
+
 
 
 def daily_forecast(woeid, year, month, day):
-    pass  # YOUR CODE HERE
+    url = urllib.parse.urljoin(BASE_URI, f"/api/location/{woeid}/{year}/{month}/{day}")
+    return requests.get(url).json()
 
 def monthly_forecast(woeid, year, month):
     """ return a `list` of forecasts for the whole month """
-    pass  # YOUR CODE HERE
+    forecasts = []
+    date = datetime.date(year, month, 1)
+    if month == 12:
+        upper_bound = datetime.date(year + 1, 1, 1)
+    else:
+        upper_bound = datetime.date(year, month + 1, 1)
+    while date < upper_bound:
+        print(f"Fetching forecast for {date.strftime('%Y-%m-%d')}")
+        forecasts += daily_forecast(woeid, date.year, date.month, date.day)
+        date = date + datetime.timedelta(days=1)
+    return forecasts
 
 def write_csv(woeid, year, month, city, forecasts):
     """ dump all the forecasts to a CSV file in the `data` folder """
-    pass  # YOUR CODE HERE
+    filename = f"{year}_{'{:02d}'.format(month)}_{woeid}_{city.lower()}"
+    with open(f"data/{filename}.csv", 'w') as output_file:
+        keys = forecasts[0].keys()
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(forecasts)
+# $CHALLENGIFY_END
 
 def main():
     if len(sys.argv) > 2:
