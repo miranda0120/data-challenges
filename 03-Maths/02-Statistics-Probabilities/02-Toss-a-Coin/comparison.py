bb8bbbc7013d4619e3768739b0a6bbc202e86d66
Meiling Wang <miranda0120@hotmail.com>
2021-11-07 18:45:14 +0000

    Completed comparison

--- 03-Maths/02-Statistics-Probabilities/02-Toss-a-Coin/comparison.py
+++ 03-Maths/02-Statistics-Probabilities/02-Toss-a-Coin/comparison.py
@@ -7,4 +7,11 @@ from simulate_reality import play_n_game
 def mean_squared_error(n_games, n_toss):
     '''TO DO: return the squared error between the theoretical and "actual"
      results (obtained through simulation)'''
-    pass
+    theoretical_results = probability(n_toss)
+    actual_results = play_n_game(n_games, n_toss)
+    squared_error = 0
+    for k, v in theoretical_results.items():
+        actual_result = actual_results[k]
+        squared_error += (actual_result - theoretical_results[k])**2
+    mse = squared_error/len(theoretical_results)
+    return mse
